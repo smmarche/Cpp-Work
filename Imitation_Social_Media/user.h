e8b21632f9f0a3c036ae1fde3339802aaa42184b
smmarche <105466339+smmarche@users.noreply.github.com>
2022-12-19 10:23:05 -0600

    Create user.h

---
+++ Imitation_Social_Media/user.h
@@ -0,0 +1,21 @@
+#ifndef _USER_H
+#define _USER_H
+
+#include <string>
+#include <vector>
+#include "Post.h"
+
+// the class definition for the network user
+
+class User {
+private:
+  std::string userName;
+  std::vector<Post*> userPosts;
+public:
+  User(std::string userName);
+  std::string getUserName();
+  std::vector<Post*>& getUserPosts();
+  void addUserPost(Post* post);
+};
+
+#endif
